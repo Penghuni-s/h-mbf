d797712514ded3deb4885b3c44c8788afe667227
Penghuni-s <86087919+Penghuni-s@users.noreply.github.com>
2021-06-19 12:03:57 +0700

    Update Haram6.py

--- Haram6.py
+++ Haram6.py
@@ -335,14 +335,21 @@ def moch_yayan():
                except requests.exceptions.ConnectionError:
                        exit("\n\n\033[00m  [\033[91m!\033[00m] no Connection")
 
-    elif yayan == "2" or yayan =="02":
-     	print("\n\n\x1b[1;97m      [ \x1b[1;92mtunggu aja lagi update \x1b[1;97m]\n")
+   
+   elif yayan == "5" or yayan =="05":
+         print("\n\n\x1b[1;97m      [ \x1b[1;92mtunggu aja lagi update \x1b[1;97m]\n")
          os.system("git pull")
          print("\n \x1b[1;97m[\x1b[1;92m√\x1b[1;97m]\x1b[1;92m sukses update!\n ")
          yayanxd()
+    elif yayan == "3" or yayan =="03":
+         os.system('')
+         yayanxd()
+    elif yayan == "4" or yayan =="04":
+         os.system('')
+         yayanxd()
     elif yayan == "0" or yayan =="00":
          aahh("\n\033[1;92m pergi sana lu anjing\n\n")
-         os.system(python CRACK.py)
+         os.system(python Haram6.py)
          exit()                   	
 
 if __name__=="__main__":
